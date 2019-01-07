60220649e1a564c7b6e16b309d30a37c8b36162d
Clarice Costello <claricostello@outlook.com>
2019-01-07 10:15:43 -0800

    defined add function

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Add(2, 3);
+        }
+        public static void Add(int x, int y)
+        {
+            int sum = x + y;
+            Console.Write("Sum = " + sum);
+            Console.ReadLine();
+        } 
+    }
+}
